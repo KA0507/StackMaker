e352bd1538c63c7a3b52667807c387ce0bac3233
KA0507 <kyanh0507@gmail.com>
2023-02-27 14:32:02 +0700

    first commit

---
+++ Assets/Scripts/StackBrick.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackBrick : MonoBehaviour
+{
+    [SerializeField] private GameObject brick;
+    [SerializeField] private Transform people;
+
+    Stack<GameObject> stackBrick = new Stack<GameObject>();
+    private int countBrick = 0;
+    Vector3 brick_y = new Vector3(0, 0.25f, 0);
+    private Transform transformPeople;
+    private void Start()
+    {
+        transformPeople = people.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Va chạm với Brick
+        if (other.gameObject.tag == "Brick")
+        {
+            // Thêm gạch
+            addBrick(other);
+        }
+        // Va chạm với Bridge
+        if (other.gameObject.tag == "Bridge")
+        {
+            // Xóa gạch
+            removeBrick(other);
+        }
+    }
+
+    private void addBrick(Collider other)
+    {
+        other.transform.SetParent(transform.parent);// Viên gạch thành con Player
+        other.transform.position = transform.position + brick_y * countBrick; // Đặt vị trí viên gạch
+        other.transform.rotation = Quaternion.Euler(new Vector3(-90, 90, 90));
+        people.transform.position = people.transform.position + brick_y; // Đặt vị trí people
+        countBrick++;
+        other.GetComponent<BoxCollider>().enabled = false; // Tắt component box collider
+        stackBrick.Push(other.gameObject); // Thêm gạch vào stack
+    }
+    private void removeBrick(Collider other)
+    {
+        // Debug.Log("hit");
+        Destroy(stackBrick.Pop()); // Xóa gạch từ stack
+        // Tạo gạch trên cầu
+        Instantiate(brick, new Vector3(other.transform.position.x, 2.55f, other.transform.position.z), Quaternion.Euler(new Vector3(-90, 0, 0)));
+        people.transform.position -= brick_y; // Đặt vị trí people
+        countBrick--;
+        Destroy(other.gameObject); // Xóa tấm vàng trên cầu
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
+}
---
+++ Assets/Scripts/SwithScene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwithScene : MonoBehaviour
+{
+    public float delay = 1.5f; // thời gian chờ trước khi chuyển sang scene tiếp theo
+    private int currentSceneIndex = 0;
+
+    private void LoadNextScene()
+    {
+        // Tăng chỉ số scene và chuyển sang scene tiếp theo trong thứ tự
+        currentSceneIndex++;
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("Không có scene tiếp theo để chuyển đến.");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Lấy chỉ số của scene hiện tại
+            currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            // Chờ một thời gian rồi chuyển sang scene tiếp theo
+            Invoke("LoadNextScene", delay);
+        }
+    }
+}
